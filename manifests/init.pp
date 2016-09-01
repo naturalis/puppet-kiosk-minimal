586939907a2423d0a121a48a967065405b5ef55a
David Heijkamp <david.heijkamp@naturalis.nl>
2016-09-01 16:48:14 +0200

    Actual fix of SSH key / remote user

--- manifests/init.pp
+++ manifests/init.pp
@@ -96,13 +96,6 @@ class kiosk_minimal(
     managehome            => true,
     password              => sha1('kiosk'),
   }
-  user { "remote":
-    comment               => "remote user",
-    home                  => "/home/remote",
-    ensure                => present,
-    managehome            => true,
-    password              => sha1('remote'),
-  }
 # startx on login
   file { '/home/kiosk/.profile':
     ensure                => present,
@@ -131,7 +124,7 @@ class kiosk_minimal(
         password          => sha1('stargazer'),
       }
     ssh_authorized_key { 'stargazer@naturalis':
-      user                => 'remote',
+      user                => 'stargazer',
       type                => 'ssh-rsa',
       key                 => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC+kdUMHaaToYsntgV5vkOvJXDBxUHLxZk7J+J6HtHZ5E7pv6O+d0ksc2aMyHaDl9F7TxUsCATDZErNozFcOx/O5om0SVypxmeewpCtBnXfdNd/HG9sUqvKCOQ/xA+nU+FHI8LtIiQQJ8yUm4GNN2sSRFigY8/GdKMTIpRh/lbBczDkXYjCb6iAV3t8pI7tTtt9z1QnC4vR6sTt7dXTN4ADkmeiIXe1tWkLjk8ptQ0BCjqUgIw1+0f8CisVsiLRJPVANOVg6wR5IlfZWX4XxydOKT/Xl6Bhf517FZC7MGYQ/6bFlUbeIM6svFxqQxcisNGrhpFhZhPXlrs2FHey426d',
     }
