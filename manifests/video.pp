ff360770846b95baa535c80ae5012f9b4d95bbcb
David Heijkamp <david.heijkamp@naturalis.nl>
2016-09-05 12:00:56 +0200

    Add video download functionality

--- manifests/video.pp
+++ manifests/video.pp
@@ -1,16 +1,17 @@
 class kiosk_minimal::video(
   $dirs                     = ['/home/kiosk/','/home/kiosk/.config','/home/kiosk/.config/openbox','/home/kiosk/.icons/','/home/kiosk/.icons/default/','/home/kiosk/.icons/default/cursors'],
-  $video                  = undef,
+  $videourl               = undef,
+  $videoname              = undef,
   $rotation               = $kiosk_minimal::rotation,
   $saturation             = '0',
   $contrast               = '0',
 )
  {
-  # install packages
+    # install packages
     package { 'mpv':
       ensure                => installed
     }
-  # make userdirs
+    # make userdirs
     file { $dirs:
       ensure                => 'directory',
       require               => User['kiosk'],
@@ -18,6 +19,28 @@ class kiosk_minimal::video(
       group                 => 'kiosk',
       mode                  => '0644'
     }
+    # Get video file from Drive
+    #file { '/home/kiosk/$videoname':
+    #  ensure                => 'file',
+    #  require               => User['kiosk'],
+    #  owner                 => 'kiosk',
+    #  group                 => 'kiosk',
+    #  mode                  => '0644',
+    #  source                => $video,
+    #}
+    exec { "/usr/bin/wget --timestamping -O $videoname $videourl":
+      alias                 => "videolatest",
+      cwd                   => "/home/kiosk",
+    }
+
+    file { "/home/kiosk/$videoname":
+      require               => [ User['kiosk'], Exec["videolatest"] ],
+      owner                 => 'kiosk',
+      group                 => 'kiosk',
+      mode                  => '0644',
+      alias                 => "video",
+    }
+
   # autostart chrome
     file { '/home/kiosk/.config/openbox/autostart.sh':
       ensure                => present,
